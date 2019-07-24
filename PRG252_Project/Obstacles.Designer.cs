265452cff51c3bafe1c4bf0d117771e73e9a9ec7
Malwandla <milongoveni@gmail.com>
2019-07-24 20:06:48 +0200

    Updated Obstacles Form and functionality

---
+++ PRG252_Project/Obstacles.Designer.cs
@@ -0,0 +1,337 @@
+﻿namespace PRG252_Project
+{
+    partial class Obstacles
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblJet = new System.Windows.Forms.Label();
+            this.lblObstacle1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblObstacle8 = new System.Windows.Forms.Label();
+            this.lblObstacle2 = new System.Windows.Forms.Label();
+            this.lblObstacle3 = new System.Windows.Forms.Label();
+            this.lblObstacle4 = new System.Windows.Forms.Label();
+            this.lblObstacle5 = new System.Windows.Forms.Label();
+            this.lblObstacle6 = new System.Windows.Forms.Label();
+            this.lblObstacle7 = new System.Windows.Forms.Label();
+            this.lblEnemyCamp = new System.Windows.Forms.Label();
+            this.lblObstacle10 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblJet
+            // 
+            this.lblJet.AutoSize = true;
+            this.lblJet.Location = new System.Drawing.Point(17, 18);
+            this.lblJet.Name = "lblJet";
+            this.lblJet.Size = new System.Drawing.Size(21, 13);
+            this.lblJet.TabIndex = 0;
+            this.lblJet.Text = "Jet";
+            // 
+            // lblObstacle1
+            // 
+            this.lblObstacle1.AutoSize = true;
+            this.lblObstacle1.Location = new System.Drawing.Point(154, 155);
+            this.lblObstacle1.Name = "lblObstacle1";
+            this.lblObstacle1.Size = new System.Drawing.Size(55, 13);
+            this.lblObstacle1.TabIndex = 1;
+            this.lblObstacle1.Text = "Obstacle1";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(111, 374);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblObstacle8
+            // 
+            this.lblObstacle8.AutoSize = true;
+            this.lblObstacle8.Location = new System.Drawing.Point(286, 88);
+            this.lblObstacle8.Name = "lblObstacle8";
+            this.lblObstacle8.Size = new System.Drawing.Size(55, 13);
+            this.lblObstacle8.TabIndex = 3;
+            this.lblObstacle8.Text = "Obstacle8";
+            // 
+            // lblObstacle2
+            // 
+            this.lblObstacle2.AutoSize = true;
+            this.lblObstacle2.Location = new System.Drawing.Point(406, 155);
+            this.lblObstacle2.Name = "lblObstacle2";
+            this.lblObstacle2.Size = new System.Drawing.Size(55, 13);
+            this.lblObstacle2.TabIndex = 4;
+            this.lblObstacle2.Text = "Obstacle2";
+            // 
+            // lblObstacle3
+            // 
+            this.lblObstacle3.AutoSize = true;
+            this.lblObstacle3.Location = new System.Drawing.Point(17, 124);
+            this.lblObstacle3.Name = "lblObstacle3";
+            this.lblObstacle3.Size = new System.Drawing.Size(55, 13);
+            this.lblObstacle3.TabIndex = 5;
+            this.lblObstacle3.Text = "Obstacle3";
+            // 
+            // lblObstacle4
+            // 
+            this.lblObstacle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblObstacle4.Location = new System.Drawing.Point(57, 287);
+            this.lblObstacle4.MaximumSize = new System.Drawing.Size(400, 200);
+            this.lblObstacle4.Name = "lblObstacle4";
+            this.lblObstacle4.Size = new System.Drawing.Size(81, 25);
+            this.lblObstacle4.TabIndex = 6;
+            this.lblObstacle4.Text = "Obstacle4";
+            // 
+            // lblObstacle5
+            // 
+            this.lblObstacle5.AutoSize = true;
+            this.lblObstacle5.Location = new System.Drawing.Point(415, 374);
+            this.lblObstacle5.Name = "lblObstacle5";
+            this.lblObstacle5.Size = new System.Drawing.Size(55, 13);
+            this.lblObstacle5.TabIndex = 7;
+            this.lblObstacle5.Text = "Obstacle5";
+            // 
+            // lblObstacle6
+            // 
+            this.lblObstacle6.AutoSize = true;
+            this.lblObstacle6.Location = new System.Drawing.Point(561, 124);
+            this.lblObstacle6.Name = "lblObstacle6";
+            this.lblObstacle6.Size = new System.Drawing.Size(55, 13);
+            this.lblObstacle6.TabIndex = 8;
+            this.lblObstacle6.Text = "Obstacle6";
+            // 
+            // lblObstacle7
+            // 
+            this.lblObstacle7.AutoSize = true;
+            this.lblObstacle7.Location = new System.Drawing.Point(676, 45);
+            this.lblObstacle7.Name = "lblObstacle7";
+            this.lblObstacle7.Size = new System.Drawing.Size(55, 13);
+            this.lblObstacle7.TabIndex = 9;
+            this.lblObstacle7.Text = "Obstacle7";
+            // 
+            // lblEnemyCamp
+            // 
+            this.lblEnemyCamp.AutoSize = true;
+            this.lblEnemyCamp.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEnemyCamp.Location = new System.Drawing.Point(784, 193);
+            this.lblEnemyCamp.Name = "lblEnemyCamp";
+            this.lblEnemyCamp.Size = new System.Drawing.Size(140, 25);
+            this.lblEnemyCamp.TabIndex = 12;
+            this.lblEnemyCamp.Text = "Enemy Camp";
+            // 
+            // lblObstacle10
+            // 
+            this.lblObstacle10.AutoSize = true;
+            this.lblObstacle10.Location = new System.Drawing.Point(148, 35);
+            this.lblObstacle10.Name = "lblObstacle10";
+            this.lblObstacle10.Size = new System.Drawing.Size(61, 13);
+            this.lblObstacle10.TabIndex = 13;
+            this.lblObstacle10.Text = "Obstacle10";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(786, 135);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(240, 268);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Obstacle11";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(459, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Obstacle12";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(459, 254);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Obstacle13";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(839, 319);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(55, 13);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "Obstacle9";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(747, 400);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(55, 13);
+            this.label8.TabIndex = 21;
+            this.label8.Text = "Obstacle9";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(656, 319);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(55, 13);
+            this.label9.TabIndex = 22;
+            this.label9.Text = "Obstacle9";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(523, 418);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(55, 13);
+            this.label10.TabIndex = 23;
+            this.label10.Text = "Obstacle9";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(689, 205);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 13);
+            this.label5.TabIndex = 24;
+            this.label5.Text = "Obstacle9";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(786, 268);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 25;
+            this.label6.Text = "label6";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(839, 103);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(55, 13);
+            this.label11.TabIndex = 26;
+            this.label11.Text = "Obstacle9";
+            // 
+            // Obstacles
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(974, 493);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblObstacle10);
+            this.Controls.Add(this.lblEnemyCamp);
+            this.Controls.Add(this.lblObstacle7);
+            this.Controls.Add(this.lblObstacle6);
+            this.Controls.Add(this.lblObstacle5);
+            this.Controls.Add(this.lblObstacle4);
+            this.Controls.Add(this.lblObstacle3);
+            this.Controls.Add(this.lblObstacle2);
+            this.Controls.Add(this.lblObstacle8);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblObstacle1);
+            this.Controls.Add(this.lblJet);
+            this.Name = "Obstacles";
+            this.Text = "Obstacles";
+            this.Load += new System.EventHandler(this.Obstacles_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblJet;
+        private System.Windows.Forms.Label lblObstacle1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblObstacle8;
+        private System.Windows.Forms.Label lblObstacle2;
+        private System.Windows.Forms.Label lblObstacle3;
+        private System.Windows.Forms.Label lblObstacle4;
+        private System.Windows.Forms.Label lblObstacle5;
+        private System.Windows.Forms.Label lblObstacle6;
+        private System.Windows.Forms.Label lblObstacle7;
+        private System.Windows.Forms.Label lblEnemyCamp;
+        private System.Windows.Forms.Label lblObstacle10;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label11;
+    }
+}
---
+++ PRG252_Project/Obstacles.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PRG252_Project
+{
+    public partial class Obstacles : Form
+    {
+        public Obstacles()
+        {
+            InitializeComponent();
+        }
+        int x, y = 0;//x and y are for moving horizontally
+        string launch = "Go";
+      
+
+
+     
+
+        private void Obstacles_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Interval = 1; //1 second
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblJet.SetBounds(x, y, 1, 1);
+            if (launch == "Go")
+            {
+                y++;
+            }
+
+            if (y == 88)
+            {
+                launch = "Evade";
+                x++; 
+            }
+                if (true)
+            {
+
+            }
+            if (x == 227)
+            {
+                y++;
+            }
+            if (y == 170) //155
+            {
+                x++;
+            }
+            if (x == 327)
+            {
+                y++;
+            }
+            if (y == 500)
+            {
+                x++;
+            }
+            if (x == 558)
+            {
+                y--; 
+            }
+            if (y == 190)
+            {
+                x++; 
+            }
+            if (x == 786)
+            {
+                y++; 
+            }
+            if (y == 220)
+            {
+                y--;
+            }
+            if (lblJet.Bounds.IntersectsWith(lblEnemyCamp.Bounds))
+            {
+                x = 868;
+                y = 198;
+                lblJet.Hide();
+                timer1.Stop();
+                MessageBox.Show("The pilot has safely entered the enemy camp zone");
+            }
+        }
+    }
+}
