c5ac7e8f4697f972e3a02f26edd151c98e3a87a3
Moagi boikanyo <moagib23@gmail.com>
2019-07-24 23:34:32 +0200

    File handler update

--- Project Demo/Project Demo/FileHandler.cs
+++ Project Demo/Project Demo/FileHandler.cs
@@ -1,12 +1,81 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
 
 namespace Project_Demo
 {
     class FileHandler
     {
+         FileStream stream;
+        StreamReader reader;
+        StreamWriter writer;
+        public List<string> ReadData(string fileName)
+        {
+            List<string> rawData = new List<string>();
+            
+            try
+            {
+                stream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+                reader = new StreamReader(stream);
+                string line = string.Empty;
+                while ((line=reader.ReadLine())!= null)
+                {
+                   
+                    rawData.Add(line);
+                }
+            }
+            catch (FileNotFoundException fnfe)
+            {
+                MessageBox.Show(fnfe.Message);
+            }
+            catch (DirectoryNotFoundException dnfe)
+            {
+                MessageBox.Show(dnfe.Message);
+            }
+            finally
+            {
+                
+                
+
+
+                reader.Close();
+                stream.Close(); 
+            }
+
+            return rawData;
+        }
+        public void WriteData(string fileName, string dataToWrite)
+        {
+            try
+            {
+                stream = new FileStream(fileName, FileMode.Append, FileAccess.Write);
+                writer = new StreamWriter(stream);
+                writer.WriteLine(dataToWrite);
+            }
+            catch (FileNotFoundException fnfe)
+            {
+                MessageBox.Show(fnfe.Message);
+            }
+            catch (DirectoryNotFoundException dnfe)
+            {
+                MessageBox.Show(dnfe.Message);
+            }
+            finally
+            {
+                
+                writer.Close();
+                stream.Close();
+            }
+
+        }
+        public void UpdateData(string fileName, string dataToUpdate)
+        {
+            stream = new FileStream(fileName, FileMode.Append, FileAccess.Write);
+
+        }
     }
 }
